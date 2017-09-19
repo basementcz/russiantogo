e44af735c8a2ea22623032757c2f3633f3379c63
basementcz <you@example.com>
2017-09-19 23:10:55 +0200

    updated js

--- js/landing-page.js
+++ js/landing-page.js
@@ -1,17 +1,17 @@
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+  $('a.page-scroll').bind('click', function(event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+          scrollTop: $($anchor.attr('href')).offset().top-25
+      }, 1500, 'easeInOutExpo');
+      event.preventDefault();
+  });
 });
 
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
-    target: '.navbar-fixed-top'
+  target: '.navbar-main'
 })
 
 // Closes the Responsive Menu on Menu Item Click
